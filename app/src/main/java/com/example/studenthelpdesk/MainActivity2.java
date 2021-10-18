c0eb54d07d316e6f65ba0e34042e820dc55e0d5a
vanshika <vanshikav.kumar@gmail.com>
2021-10-19 02:24:36 +0530

    Mansi testing done via code share

---
+++ app/src/main/java/com/example/studenthelpdesk/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.studenthelpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
+    }
+}
