0665db406d4f54fc92869167843e31671ee77963
vanshika <vanshikav.kumar@gmail.com>
2021-10-20 00:39:20 +0530

    Mansi commit

--- app/src/main/java/com/example/studenthelpdesk/Admin_page.java
+++ app/src/main/java/com/example/studenthelpdesk/Admin_page.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Admin_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-         button=(Button)findViewById(R.id.textView);
-button.set
+        setContentView(R.layout.activity_admin_page);
+        Button create_new_member=(Button) findViewById(R.id.createnew);
+
     }
 }
