a081061cd1afac60702d214ea0567b39c4edac32
vanshika <vanshikav.kumar@gmail.com>
2021-10-20 01:59:08 +0530

    Login

--- app/src/main/java/com/example/studenthelpdesk/CreateStudents.java
+++ app/src/main/java/com/example/studenthelpdesk/CreateStudents.java
@@ -1,12 +1,17 @@
 package com.example.studenthelpdesk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -19,13 +24,19 @@ public class CreateStudents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_students);
-        
-        //here
+
+        EditText unameet=findViewById(R.id.uname);
+String uname= unameet.getText().toString();
+
+EditText Email=findViewById(R.id.email);
+String email= Email.getText().toString();
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, Object> user = new HashMap<>();
-        
+        user.put(uname,email);
+        db.collection("AllowedUser").add(uname).add
+
     }
 
     public void onStart() {
@@ -33,7 +44,7 @@ public class CreateStudents extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser==null){
-            
+
             Intent intent = new Intent(CreateStudents.this,LoginActivity.class);
         startActivity(intent);
         }
