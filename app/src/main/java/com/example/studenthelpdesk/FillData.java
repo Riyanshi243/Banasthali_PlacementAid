ac9ac6aa2749510fff1f83564677e59d2eb8e4cc
Riyanshi243 <riyanshiverma243@gmail.com>
2021-10-21 01:43:27 +0530

    background chances done2

---
+++ app/src/main/java/com/example/studenthelpdesk/FillData.java
@@ -0,0 +1,15 @@
+package com.example.studenthelpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FillData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_data);
+        
+    }
+}
