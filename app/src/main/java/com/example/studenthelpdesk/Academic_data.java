8ff5926708dde3a2b40a7897487af908e076f6f5
vanshika <vanshikav.kumar@gmail.com>
2021-10-23 01:13:25 +0530

    My changes

--- app/src/main/java/com/example/studenthelpdesk/Academic_data.java
+++ app/src/main/java/com/example/studenthelpdesk/Academic_data.java
@@ -3,13 +3,15 @@ package com.example.studenthelpdesk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Academic_data extends AppCompatActivity {
-private EditText course,branch,rollno,erollno,cgpa,tenth
+private EditText course,branch,rollno,erollno,cgpa,tenth,twelth,dob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academic_data);
+        dob=(EditText) findViewById(R.id.dob);
     }
 }
