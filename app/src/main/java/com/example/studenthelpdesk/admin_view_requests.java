e1815cef8264e2ebcde006ab84dace16aee3d910
vanshika <vanshikav.kumar@gmail.com>
2021-11-17 00:31:05 +0530

    Chhote mote changes

--- app/src/main/java/com/example/studenthelpdesk/admin_view_requests.java
+++ app/src/main/java/com/example/studenthelpdesk/admin_view_requests.java
@@ -1,10 +1,16 @@
 package com.example.studenthelpdesk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class admin_view_requests extends AppCompatActivity {
 
@@ -18,15 +24,43 @@ public class admin_view_requests extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         //intent to admin page
-        Intent intent = new Intent(admin_view_requests.this,Admin_page.class);
+        Intent intent = new Intent(admin_view_requests.this, Admin_page.class);
         startActivity(intent);
         finish();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        getMenuInflater().inflate(R.menu.main,menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                AlertDialog.Builder ab = new AlertDialog.Builder(this);
+                ab.setTitle("LOGOUT");
+                ab.setMessage("Are you sure?");
+                ab.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //logut
+                        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                        firebaseAuth.signOut();
+                        Intent intent = new Intent(admin_view_requests.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).setNegativeButton("No ", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //do nothing;
+                    }
+                });
+                ab.create().show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
